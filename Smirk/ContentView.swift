9b711842d2a20195f3f326907aa79e5ef57ef0b8
Moazam5 <m.moazam29@gmail.com>
2021-01-29 21:14:26 -0500

    Initial Commit

---
+++ Smirk/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Smirk
+//
+//  Created by Moazam Mir on 1/29/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Smirk/SmirkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmirkApp.swift
+//  Smirk
+//
+//  Created by Moazam Mir on 1/29/21.
+//
+
+import SwiftUI
+
+@main
+struct SmirkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
