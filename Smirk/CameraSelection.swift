73a40c9e59ffb5449af7c471ebbbd1c49ac7bfc4
moazam5 <m.moazam29@gmail.com>
2021-03-07 15:17:46 -0500

    add progress bar

--- Smirk/CameraSelection.swift
+++ Smirk/CameraSelection.swift
@@ -8,13 +8,60 @@
 import SwiftUI
 
 struct CameraSelection: View {
+    @State private var downloadAmount = 0.0
+   // let timer = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
+
+    @State var showPV = true
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-        
+        VStack {
+            ZStack{
+                //ProgressView(value: 0.25)
+                Divider().frame(width: 200, alignment: .center)
+                    .background(Color.white)
+                HStack{
+                    Text("Setup Photo")
+                    .padding()
+
+                    .background(Color.white)
+                    .cornerRadius(30)
+                    Divider().frame(width: 300, height: 3, alignment: .center)
+                        .background(Color.white)
+                   // Spacer()
+                    
+                    Text("Setup Printer")
+                        .foregroundColor(Color.white)
+                        .cornerRadius(6)
+                        .padding()
+                        .overlay(
+                               RoundedRectangle(cornerRadius: 30)
+                                   .stroke(Color.white, lineWidth: 6)
+                           )
+                       
+                }
+                
+                    
+              
+
+            }
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+    
+    //    .progressViewStyle(DarkBlueShadowProgressViewStyle())
+        .background(LinearGradient(gradient: Gradient(colors: [.white, .blue]), startPoint: .top, endPoint: .bottom))
+        .ignoresSafeArea()
         
     }
-}
+    
+    }
 
+struct DarkBlueShadowProgressViewStyle: ProgressViewStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        ProgressView(configuration)
+            .shadow(color: Color(red: 0, green: 0, blue: 0.6),
+                    radius: 4.0, x: 1.0, y: 2.0)
+    }
+}
 struct CameraSelection_Previews: PreviewProvider {
     static var previews: some View {
         CameraSelection()
